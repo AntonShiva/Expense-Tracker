f591df129ca45a65a238985be4fa64b625e71d50
AntonShiva <ryashinanthon@yandex.ru>
2023-10-16 16:58:53 +0300

    создал расширение для Color

---
+++ Expense Tracker/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  Expense Tracker
+//
+//  Created by Anton Rasen on 16.10.2023.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let backgoundColor = Color("Backgound")
+    static let iconColor = Color("Icon")
+    static let textColor = Color("Text")
+    static let systemBackground = Color(uiColor: .systemBackground)
+}
