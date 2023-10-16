f67090510000930de76f36c73f211035cd0aeb0a
AntonShiva <ryashinanthon@yandex.ru>
2023-10-16 12:22:23 +0300

    Initial Commit

---
+++ Expense Tracker/Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_TrackerApp.swift
+//  Expense Tracker
+//
+//  Created by Anton Rasen on 16.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
