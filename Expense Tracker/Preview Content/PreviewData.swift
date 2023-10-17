4edd4081a2b45d938dfa08bdb31aefee6e9f2481
AntonShiva <ryashinanthon@yandex.ru>
2023-10-17 20:11:19 +0300

    создал PreviewData

---
+++ Expense Tracker/Preview Content/PreviewData.swift
@@ -0,0 +1,13 @@
+//
+//  PreviewData.swift
+//  Expense Tracker
+//
+//  Created by Anton Rasen on 17.10.2023.
+//
+
+import Foundation
+import SwiftUI
+ 
+var transactionPreviewData = Transaction(id: 1, date: "01/24/2022", institution: "Desjardins", account: "Visa Desjardins", maerchant: "Apple", amount: 11.49, type: "debit", categoryId: 801, category: "Software", isPending: false, isTransfer: false, isExpense: true, isEdited: false)
+
+var transactionListPreviewData = [Transaction](repeating: transactionPreviewData, count: 10)
